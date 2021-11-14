ffc6a192ceaaa6caded45a638038a774f12dc235
Benjamin DANIEL <benjamindaniel902@gmail.com>
2021-11-14 17:20:26 +0100

    conf: Fix base href for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,7 @@ import { PublicConf } from './public-conf.models';
 
 export const environment = _.merge({}, base.public, {
 	name: 'prod',
+	api: {
+		baseHref: '/serv-daniel/kdos/',
+	},
 } as PublicConf);
